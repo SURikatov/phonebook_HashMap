79e3e10c46068489a633631f6389c9fcd41de4bd
SURikatov <153837408+SURikatov@users.noreply.github.com>
2024-04-26 18:55:13 +0500

    добавил возможность удалить контакт из списка

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,13 +1,24 @@
 import java.util.*;
 
-public class PhoneBook {
+public class PhoneBook 
+{
     private Map<String, Set<String>> contacts;
-
-    public PhoneBook() {
+    public PhoneBook() 
+    {
         contacts = new HashMap<>();
     }
-    public void addContact(String name, String phoneNumber) {
-    contacts.putIfAbsent(name, new HashSet<>());
-    contacts.get(name).add(phoneNumber);
+    public void addContact(String name, String phoneNumber) 
+    {
+        contacts.putIfAbsent(name, new HashSet<>());
+        contacts.get(name).add(phoneNumber);
+    }
+    public void removeContact(String name) 
+    {
+        if (!contacts.containsKey(name)) 
+        {
+            System.out.println("Контакт " + name + " не найден.");
+            return;
+        }
+        contacts.remove(name);
     }
 }
